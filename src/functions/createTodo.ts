d2c87706bef58744286f61b23badcad159d68f95
Amaury Euzebio <amaury@a1tecnologia.info>
2021-06-07 16:06:54 -0300

    Implementação função criar todo

--- src/functions/createTodo.ts
+++ src/functions/createTodo.ts
@@ -1,10 +1,40 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 
+import { v4 as uuid } from "uuid";
+import { document } from "../utils/dynamodbClient";
+
 export const handle: APIGatewayProxyHandler = async (event) => {
+  const { user_id } = event.pathParameters;
+  const { title, deadline } = JSON.parse(event.body);
+
+  const todo_id = uuid();
+
+  console.log(todo_id, title, deadline);
+
+  await document
+    .put({
+      TableName: "todos",
+      Item: {
+        id: todo_id,
+        user_id,
+        title,
+        done: false,
+        deadline: new Date(deadline),
+      },
+    })
+    .promise();
+
   return {
     statusCode: 201,
     body: JSON.stringify({
-      message: "Create Todo",
+      message: "Todo created successfully!",
+      todo: {
+        id: todo_id,
+        user_id,
+        title,
+        done: false,
+        deadline: new Date(deadline),
+      },
     }),
     headers: {
       "Content-Type": "application/json",
